93bb478ee9861b7eca09e5ce70ca6fbb8d652d48
Ugo Anayo <Ugo.Anayo@sterling.ng>
2020-09-30 22:19:26 +0100

    set intent type in DetailsActivity

--- app/src/main/java/com/pluralsight/candycoded/DetailActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/DetailActivity.java
@@ -80,5 +80,6 @@ public class DetailActivity extends AppCompatActivity {
 
     private void createShareIntent() {
         Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
     }
 }
